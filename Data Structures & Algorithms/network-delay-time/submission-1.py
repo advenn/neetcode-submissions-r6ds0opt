149e5636578d0ec8d39a74e38d691002e7f998a7
Asadbek <qurbonovasadbek@gmail.com>
2026-05-30 23:20:25 +0200

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        maxint = sys.maxsize
+        graph = defaultdict(list)
+        for src, dest, weight in times:
+            graph[src].append((weight, dest))
+
+        dist = [float('inf')] * (n + 1)
+        dist[k] = 0
+        heap = [(0, k)]
+        
+        while heap:
+            weight, src = heapq.heappop(heap)
+
+            if weight > dist[src]:
+                continue
+
+            for cost, dest in graph[src]:
+                new_cost = cost + weight
+                if new_cost < dist[dest]:
+                    dist[dest] = new_cost
+                    heapq.heappush(heap, (new_cost, dest))
+        print(dist)
+        m = max(dist[1:])
+        return -1 if m == float("inf") else m
